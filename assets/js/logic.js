b044d06733e1b8a568794abf2017e2147900d2a9
zaitken <zaraaitken18@gmail.com>
2022-12-03 14:10:54 +0000

    adds questions to js file and adds function to loop through questions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,14 +1,46 @@
+var currentQuestionIndex = 0;
+
 // Add event listener to Start button
- var btn = document.getElementById('start');
- var questions = document.getElementById('questions');
- var startScreen = document.getElementById('start-screen');
+var btn = document.getElementById('start');
+var questionsWrapper = document.getElementById('questions');
+var startScreen = document.getElementById('start-screen');
+var questionTitle = document.getElementById('question-title');
+var choicesOutput = document.getElementById('choices');
  
- btn.addEventListener('click', showQuizContent);
+btn.addEventListener('click', showQuizContent);
 
  function showQuizContent (){
-    questions.style.display = 'block';
+    questionsWrapper.style.display = 'block';
     startScreen.style.display = 'none';
+    
+    var currentQuestion = quizQuestions[currentQuestionIndex];
+    var choices = currentQuestion.choices;
+
+    questionTitle.innerText = currentQuestion.title;
+
+    choicesOutput.innerHTML = '';
+
+    for (var i = 0; i < choices.length; i++) {
+        var choice = choices[i];
+        var isCorrect = currentQuestion.answer === choice;
+
+        choicesOutput.insertAdjacentHTML('beforeend', `
+        <button data-correct=${isCorrect}>${choice}</button>
+        `)
+    }
+
+    questionsWrapper.classList.remove('hide');
  }
 
+ function checkAnswer(event){
+    console.log('event.target');
+ }
 
- // if div is display none, change to display block
+ choicesOutput.addEventListener('click', checkAnswer);
+
+
+// add click event listener to choices div as it is the parent element
+// if class = correct, show message 'correct', else show 'wrong'
+// store result
+// detract from timer
+// show next question
---
+++ assets/js/questions.js
@@ -0,0 +1,22 @@
+var quizQuestions = [
+    {
+        title: 'Which HTML element holds the JavaScript?',
+        choices: ['Head', 'Footer', 'Script', 'Div'],
+        answer: 'Script'
+    },
+    {
+        title: 'Strings are surrounded by what?',
+        choices: ['Quotes', 'Curly brackets', 'Hashtags', 'Square brackets'],
+        answer: 'Quotes'
+    },
+    {
+        title: 'From which number do index numbers always begin?',
+        choices: [-1, 0, 1, 2],
+        answer: 0
+    },
+    {
+        title: 'Which timer runs continually until stopped?',
+        choices: ['setTimer', 'setInterval', 'startTimer', 'setTimeout'],
+        answer: 'setInterval'
+    }
+];
